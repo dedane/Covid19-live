828d0111f795cb3a327c830c01e1c12b162c778f
dedane <evanskeema@gmail.com>
2020-05-19 15:30:31 +0300

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import News from '../views/News.vue';
+import Test from '../views/test.vue';
+import Maps from '../views/maps.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +14,19 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/News',
+    name: 'News',
+    component: News,
+  },
+  {
+    path: '/Test',
+    name: 'Test',
+    component: Test,
+  },
+  {
+    path: '/Maps',
+    name: 'Maps',
+    component: Maps,
   },
 ];
 
---
+++ src/views/maps.vue
@@ -0,0 +1,15 @@
+<template>
+<div id='maps'>
+  <h1>Maps</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'maps',
+}
+</script>
+
+<style>
+
+</style>
