64e3f920b9d8ff62377817d428e0ea27e31699e5
dedane <evanskeema@gmail.com>
2020-05-20 23:11:12 +0300

    how it spreads

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,6 +51,55 @@
           </v-col>
         </v-row>
       </v-col>
+      <v-row class='justify-center align-center'>
+        <v-card class='text-center' flat>
+        <v-card-title class='text-center' ma-2>How covid Spreads</v-card-title>
+        </v-card>
+      </v-row>
+      <v-row class='justify-center align-center'>
+        <v-card color='#9a2222' class='text-center' dark>
+          <v-card-sub-title>
+            Symptomatic symptoms
+          </v-card-sub-title>
+          <v-img src='https://ik.imagekit.io/ugyodiq15/cough_No0M89PchPs.svg'></v-img>
+          <v-card-text >
+            This means that you get
+            infected by someone who has
+            already contracted covid 19.
+            This will mostly be coughed
+            or sneezed droplets.
+          </v-card-text>
+        </v-card>
+      </v-row>
+      <v-row class='justify-center align-center'>
+        <v-card color='#4e793f' class='text-center' dark>
+          <v-card-sub-title>
+            Pre-Symptomatic Symptoms
+          </v-card-sub-title>
+          <v-img src='https://ik.imagekit.io/ugyodiq15/sick_5s39IwrhvaI.svg'></v-img>
+          <v-card-text>
+            The pre-symptomatic period
+            is the one of the hardest to
+            recognise. one has to await the incubation days
+            for them to confirm from the lab whether they are infected or not
+          </v-card-text>
+        </v-card>
+      </v-row>
+      <v-row class='justify-center align-center'>
+        <v-card color='#1a61a3' class='text-center' dark>
+          <v-card-sub-title>
+            Asymptoatic Symptoms
+          </v-card-sub-title>
+          <v-img src='https://ik.imagekit.io/ugyodiq15/research-virus_TA0y8IKF3EX.svg'></v-img>
+          <v-card-text>
+            Asymptomatic individuals are infected people who
+            essentially carry the virus but dont display any
+            inherent symptoms since the disease remains dormant in
+            their bodies, its extremely rare and can only be recognized via
+            lab test.
+          </v-card-text>
+        </v-card>
+      </v-row>
     </v-content>
     <Footer></Footer>
   </div>
