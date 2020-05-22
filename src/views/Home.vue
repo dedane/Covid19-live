a8f74aee89421c597d5e2d6cc31b3edc234a1934
dedane <evanskeema@gmail.com>
2020-05-22 22:51:08 +0300

    axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,8 +39,10 @@
                 What is Covid19
               </v-card-title>
               <v-card-text>
-                Corona Viruses are A type of Virus. There are many different kinds.A newly identified type
-                originally from wuhan china has caused a recent outbreak of respiratory diseases called COVID-19
+                Corona Viruses are A type of Virus.
+                There are many different kinds.A newly identified type
+                originally from wuhan china has caused a recent outbreak
+                of respiratory diseases called COVID-19
               </v-card-text>
               <v-card-actions>
                 <v-btn rounded color="#9a2222" dark>
@@ -90,12 +92,15 @@
         </v-card>
       </v-col>
       </v-row>
+       <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
     </v-content>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Mapbox from 'mapbox-gl';
+import { MglMap } from 'vue-mapbox';
 import Header from '@/components/NavBar.vue';
 import Footer from '@/components/Footer.vue';
 
@@ -103,24 +108,39 @@ export default {
   components: {
     Header,
     Footer,
+    MglMap,
   },
   data: () => ({
+    accessToken: 'pk.eyJ1IjoiZXZhbnM2NjYiLCJhIjoiY2thaDllZno5MGUzYjJxdDllMmdhYjVoYiJ9.3oJSZ6QiZe3t0kugy69hmw',
+    mapStyle: 'dark-v10',
     spreads: [
-      {title: 'Symptomatic symptoms',img: 'https://ik.imagekit.io/ugyodiq15/cough_No0M89PchPs.svg', 
-      text: 'This means that you get infected by someone who has already contracted covid 19. This will mostly be coughed or sneezed droplets.'},
-      {title: 'Pre-Symptomatic Symptoms',img: 'https://ik.imagekit.io/ugyodiq15/sick_5s39IwrhvaI.svg',
-      text: 'The pre-symptomatic period is the one of the hardest to recognise. one has to await the incubation days for them to confirm from the lab whether they are infected or not'},
-      {title: 'Asymptoatic Symptoms',img: 'https://ik.imagekit.io/ugyodiq15/research-virus_TA0y8IKF3EX.svg',
-      text: 'Asymptomatic individuals are infected people who essentially carry the virus but dont display any inherent symptoms since the disease remains dormant in their bodies, its extremely rare and can only be recognized via lab test.'},
+      {
+        title: 'Symptomatic symptoms',
+        img: 'https://ik.imagekit.io/ugyodiq15/cough_No0M89PchPs.svg',
+        text: 'This means that you get infected by someone who has already contracted covid 19. This will mostly be coughed or sneezed droplets.',
+      },
+      {
+        title: 'Pre-Symptomatic Symptoms',
+        img: 'https://ik.imagekit.io/ugyodiq15/sick_5s39IwrhvaI.svg',
+        text: 'The pre-symptomatic period is the one of the hardest to recognise. one has to await the incubation days for them to confirm from the lab whether they are infected or not',
+      },
+      {
+        title: 'Asymptoatic Symptoms',
+        img: 'https://ik.imagekit.io/ugyodiq15/research-virus_TA0y8IKF3EX.svg',
+        text: 'Asymptomatic individuals are infected people who essentially carry the virus but dont display any inherent symptoms since the disease remains dormant in their bodies, its extremely rare and can only be recognized via lab test.',
+      },
     ],
     avoids: [
-      {img: 'https://ik.imagekit.io/ugyodiq15/facial-mask_G3rp6wEiGxd.svg',text: 'Wear masks'},
-      {img: 'https://ik.imagekit.io/ugyodiq15/stay-home_1M2cOY2NAkK.svg',text: 'Stay Home'},
-      {img: 'https://ik.imagekit.io/ugyodiq15/avoid-crowd_N6YAgJM-5qZ.svg',text: 'Avoid Crowds'},
-      {img: 'https://ik.imagekit.io/ugyodiq15/social-distance_TZ_nX945z9f.svg',text: 'Social Distance'},
-      {img: 'https://ik.imagekit.io/ugyodiq15/hand-cleaning_YBYOpnHpxo2.svg',text: 'Wash Hands Oftenly'},
-    ]
-  })
+      { img: 'https://ik.imagekit.io/ugyodiq15/facial-mask_G3rp6wEiGxd.svg', text: 'Wear masks' },
+      { img: 'https://ik.imagekit.io/ugyodiq15/stay-home_1M2cOY2NAkK.svg', text: 'Stay Home' },
+      { img: 'https://ik.imagekit.io/ugyodiq15/avoid-crowd_N6YAgJM-5qZ.svg', text: 'Avoid Crowds' },
+      { img: 'https://ik.imagekit.io/ugyodiq15/social-distance_TZ_nX945z9f.svg', text: 'Social Distance' },
+      { img: 'https://ik.imagekit.io/ugyodiq15/hand-cleaning_YBYOpnHpxo2.svg', text: 'Wash Hands Oftenly' },
+    ],
+  }),
+  created() {
+    this.mapbox = Mapbox;
+  },
 
 };
 </script>
