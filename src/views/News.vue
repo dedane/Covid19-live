fb19402b695f94d0da9a21ec3d8deb2853546f3b
dedane <evanskeema@gmail.com>
2020-05-27 06:05:03 +0300

    news

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,6 +1,13 @@
 <template>
   <div id='News'>
     <Header></Header>
+    <v-container fill-height>
+      <v-row align='center' justify='center'>
+        <v-col>
+          <v-img src='https://ik.imagekit.io/ugyodiq15/Group_35_iGcYmvx4nr.svg'></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
     <Footer></Footer>
   </div>
 </template>
